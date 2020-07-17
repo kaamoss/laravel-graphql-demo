df540a7ae8c2ff883f7df9b893dba7306cc62cc8
Matthew FitzPatrick <matthew@fitzpatricksoftware.com>
2020-07-17 13:50:02 -0700

    The start of repository and entity for User

--- app/Entities/User.php
+++ app/Entities/User.php
@@ -3,8 +3,145 @@
 
 namespace App\Entities;
 
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 
-class User
-{
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="user")
+ */
+class User {
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(type="integer", name="id")
+     */
+    protected $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", nullable=false)
+     */
+    protected $email;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=true, name="first_name")
+     */
+    protected $firstName;
+
+    /**
+     * @var string|null
+     * @ORM\Column(type="string", nullable=true, name="last_name")
+     */
+    protected $lastName;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="created_at", nullable=false)
+     */
+    protected $createdAt;
+
+    /**
+     * @var \DateTime
+     * @ORM\Column(type="datetime", name="updated_at", nullable=false)
+     */
+    protected $updatedAt;
+
+    public function __construct(?int $id = null) {
+        if($id > 0) {
+            $this->id = $id;
+        }
+    }
+
+    /**
+     * @return int|null
+     */
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string|null $firstName
+     */
+    public function setFirstName(?string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string|null $lastName
+     */
+    public function setLastName(?string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getCreatedAt(): DateTime
+    {
+        return $this->createdAt;
+    }
+
+    /**
+     * @param DateTime $createdAt
+     */
+    public function setCreatedAt(DateTime $createdAt): void
+    {
+        $this->createdAt = $createdAt;
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getUpdatedAt(): DateTime
+    {
+        return $this->updatedAt;
+    }
+
+    /**
+     * @param DateTime $updatedAt
+     */
+    public function setUpdatedAt(DateTime $updatedAt): void
+    {
+        $this->updatedAt = $updatedAt;
+    }
 
 }
--- app/Repositories/DbUserRepository.php
+++ app/Repositories/DbUserRepository.php
@@ -3,7 +3,47 @@
 
 namespace App\Repositories;
 
+use App\Entities\User;
+use App\Interfaces\Repositories\IUserRepository;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\EntityRepository;
 
-class DbUserRepository {
+class DbUserRepository extends EntityRepository implements IUserRepository {
+    private $entityManager;
 
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+        parent::__construct($this->entityManager, $this->entityManager->getClassMetadata(User::class));
+    }
+
+    public function getAllUsers(): Collection {
+        $dql = "SELECT u FROM App\\Entities\\User u ORDER BY u.lastName, u.firstName";
+
+        $users = $this->entityManager->createQuery($dql)->getResult();
+        return $users;
+    }
+
+    public function getUserById($id): ?User {
+        $dql = "SELECT u FROM App\\Entities\\User u WHERE u.id = ?1";
+
+        $user = $this->entityManager->createQuery($dql)
+            ->setParameter(1, $id)
+            // ->useQueryCache(false)
+            // ->useResultCache(false)
+            ->getOneOrNullResult();
+        return $user;
+    }
+
+    public function save(User $user): User {
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+        return $user;
+    }
+
+    public function delete(User $user): User {
+        $this->entityManager->remove($user);
+        $this->entityManager->flush();
+        return $user;
+    }
 }
